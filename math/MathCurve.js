2652628f1db5cc78ca5b7dd36387046fc6e9989d
sebriko <sebriko@freenet.de>
2024-03-04 22:38:09 +0100

    Code converted to module syntax

--- math/MathCurve.js
+++ math/MathCurve.js
@@ -1,20 +1,15 @@
-// Definiere die MathCurve-Klasse
-class MathCurve extends createjs.Shape {
-    // Konstruktor mit Kurvenfunktion und Farbe als Parameter
+// Exportiere die MathCurve-Klasse als Modul
+export class MathCurve extends createjs.Shape {
     constructor(equation, color) {
         super();
-        this.graphics.setStrokeStyle(2); // Setze die Strichstärke auf 2
-
-        // Übergebe die Funktion und Farbe an die drawCurve-Methode
+        this.graphics.setStrokeStyle(2);
         this.drawCurve(equation, color);
     }
 
-    // Methode zum Zeichnen einer mathematischen Kurve
     drawCurve(equation, color) {
-        this.graphics.clear(); // Lösche vorherige Grafiken
-        this.graphics.beginStroke(color); // Setze die Strichfarbe
+        this.graphics.clear();
+        this.graphics.beginStroke(color);
 
-        // Zeichne die Kurve basierend auf der Gleichung
         for (let x = 0; x <= stage.canvas.width; x += 1) {
             const y = equation(x);
             if (x === 0) {
@@ -24,10 +19,9 @@ class MathCurve extends createjs.Shape {
             }
         }
 
-        this.graphics.endStroke(); // Beende den Strich
+        this.graphics.endStroke();
 
-        // Füge die Kurve zur Bühne hinzu
         stage.addChild(this);
-        stage.update(); // Aktualisiere die Bühne, um die Änderungen zu sehen
+        stage.update();
     }
 }
