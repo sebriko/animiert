bdd639f683e19378e1c8953a22721e24667aba0e
sebriko <sebriko@freenet.de>
2024-03-07 23:10:12 +0100

    Line thickness implemented

--- math/MathCurve.js
+++ math/MathCurve.js
@@ -1,29 +1,28 @@
-// Exportiere die MathCurve-Klasse als Modul
 export class MathCurve extends createjs.Shape {
-    constructor(equation, startX, endX, scaleX, color) {
+    constructor(equation, startX, endX, scaleX, color=createjs.Graphics.getRGB(255, 0, 0), thickness=1) {
         super();
         this.equation = equation;
         this.color = color;
         this.startX = startX;
         this.endX = endX;
-		
-		this.scaleX = scaleX;
+        this.scaleX = scaleX;
+        this.thickness = thickness; // Neue Variable für Strichstärke
 
-        this.graphics.setStrokeStyle(2);
+        this.graphics.setStrokeStyle(this.thickness); // Setze die Strichstärke im Konstruktor
         this.drawCurve();
     }
 
     drawCurve() {
         this.graphics.clear();
         this.graphics.beginStroke(this.color);
+        this.graphics.setStrokeStyle(this.thickness); // Setze die Strichstärke in der Methode
 
         for (let x = this.startX; x <= this.endX; x += 1) {
-			
             const y = this.equation(x);
             if (x === this.endX) {
-                this.graphics.moveTo(x*this.scaleX, y);
+                this.graphics.moveTo(x * this.scaleX, y);
             } else {
-                this.graphics.lineTo(x*this.scaleX, y);
+                this.graphics.lineTo(x * this.scaleX, y);
             }
         }
 
@@ -40,6 +39,12 @@ export class MathCurve extends createjs.Shape {
 
     setEndX(endX) {
         this.endX = endX;
+        this.drawCurve();
+    }
+
+    setStrokeThickness(thickness) {
+        this.thickness = thickness;
+        this.graphics.setStrokeStyle(this.thickness); // Setze die Strichstärke neu
         this.drawCurve();
     }
 }
