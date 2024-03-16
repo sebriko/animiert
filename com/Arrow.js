207b2eb7c1fb0d248bf907e49e670c8a3b7b13ea
sebriko <sebriko@freenet.de>
2024-03-16 14:16:33 +0100

    Arrors having fillings now

--- com/Arrow.js
+++ com/Arrow.js
@@ -19,43 +19,40 @@ export class Arrow extends createjs.Container {
         stage.addChild(this); // Füge den Container zur Bühne hinzu
     }
 
-    drawArrow() {
-        this.arrow.graphics.clear();
-        this.arrow.graphics.beginStroke(this.color);
-        this.arrow.graphics.setStrokeStyle(this.thickness);
+   drawArrow() {
+    this.arrow.graphics.clear();
+    this.arrow.graphics.beginStroke(this.color);
+    this.arrow.graphics.beginFill(this.color); // Setze die Füllfarbe
 
-        // Berechne die Länge und Rotation des Pfeils
-        const dx = this.endX - this.startX;
-        const dy = this.endY - this.startY;
-        const arrowLength = Math.sqrt(dx * dx + dy * dy);
-        const originalRotation = Math.atan2(dy, dx)*180/Math.PI;
-		
-		
+    this.arrow.graphics.setStrokeStyle(this.thickness);
 
-        // Zeichne die Linie des Pfeils
-        this.arrow.graphics.moveTo(0, 0);
-        this.arrow.graphics.lineTo(arrowLength,0);
+    // Berechne die Länge und Rotation des Pfeils
+    const dx = this.endX - this.startX;
+    const dy = this.endY - this.startY;
+    const arrowLength = Math.sqrt(dx * dx + dy * dy);
+    const originalRotation = Math.atan2(dy, dx)*180/Math.PI;
 
-        // Zeichne die Pfeilspitze 
-        this.arrow.graphics.moveTo(arrowLength, 0);
-        this.arrow.graphics.lineTo(arrowLength-this.size, -this.ratio);
-		this.arrow.graphics.lineTo(arrowLength-this.size, this.ratio);
-		this.arrow.graphics.lineTo(arrowLength, 0);
+    // Zeichne die Linie des Pfeils
+    this.arrow.graphics.moveTo(0, 0);
+    this.arrow.graphics.lineTo(arrowLength,0);
 
-        this.arrow.graphics.endStroke();
-		
-		
-		this.arrow.x = this.startX;
-		this.arrow.y = this.startY;
-		
-		
-		
-		this.arrow.rotation = originalRotation;
-		
+    // Zeichne die Pfeilspitze 
+    this.arrow.graphics.moveTo(arrowLength, 0);
+    this.arrow.graphics.lineTo(arrowLength-this.size, -this.ratio);
+    this.arrow.graphics.lineTo(arrowLength-this.size, this.ratio);
+    this.arrow.graphics.lineTo(arrowLength, 0);
 
-        stage.update();
-		
-    }
+    this.arrow.graphics.endFill(); // Beende die Füllung
+    this.arrow.graphics.endStroke();
+    
+    this.arrow.x = this.startX;
+    this.arrow.y = this.startY;
+    
+    this.arrow.rotation = originalRotation;
+
+    stage.update();
+}
+
 
     setStartEnd(startX, startY, endX, endY) {
         this.startX = startX;
