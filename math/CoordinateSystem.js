1029fea0d7e9b3542b7ed99759147e2bfcc4e3ed
sebriko <sebriko@freenet.de>
2024-03-18 23:55:04 +0100

    Arrowhead in coordinate system improved

--- math/CoordinateSystem.js
+++ math/CoordinateSystem.js
@@ -1,13 +1,16 @@
 import { Arrow } from '../com/Arrow.js';
 
 export class CoordinateSystem extends createjs.Container {
-    constructor(sizeTop, sizeBottom, sizeLeft, sizeRight, color = createjs.Graphics.getRGB(255, 0, 0), thickness = 1) {
+    constructor(sizeTop, sizeBottom, sizeLeft, sizeRight, color = createjs.Graphics.getRGB(255, 0, 0), arrowLength, arrowWidth, thickness = 1) {
         super();
 		
         this.sizeTop = sizeTop;
         this.sizeBottom = sizeBottom;
         this.sizeLeft = sizeLeft;
         this.sizeRight = sizeRight;
+		
+		this.arrowLength = arrowLength;
+        this.arrowWidth = arrowWidth;
 		
         this.color = color;
         this.thickness = thickness;
@@ -17,22 +20,24 @@ export class CoordinateSystem extends createjs.Container {
     }
 
     drawCoordinateSystem() {
+		
         if (this.sizeTop != 0) {
-            this.arrowTop = new Arrow(0, 0, 0, -this.sizeTop, this.color, 10, 2, 1);
+            this.arrowTop = new Arrow(0, 0, 0, -this.sizeTop, this.color, this.arrowLength, this.arrowWidth, 1);
             this.addChild(this.arrowTop);
         }
-        if (this.sizeBottom != 0) {
 		
-            this.arrowTop = new Arrow(0, 0, 0, this.sizeBottom, this.color, 10, 2, 1);
+        if (this.sizeBottom != 0) {
+		    this.arrowTop = new Arrow(0, 0, 0, this.sizeBottom, this.color, this.arrowLength, this.arrowWidth, 1);
             this.addChild(this.arrowTop);
         }		
+		
 		        if (this.sizeLeft!= 0) {
 				
-            this.arrowLeft = new Arrow(0, 0, -this.sizeLeft, 0, this.color, 10, 2, 1);
+            this.arrowLeft = new Arrow(0, 0, -this.sizeLeft, 0, this.color, this.arrowLength, this.arrowWidth, 1);
             this.addChild(this.arrowLeft);
         }		
 				        if (this.sizeRight!= 0) {
-            this.arrowRight = new Arrow(0, 0, this.sizeRight, 0, this.color, 10, 2, 1);
+            this.arrowRight = new Arrow(0, 0, this.sizeRight, 0, this.color, this.arrowLength, this.arrowWidth, 1);
             this.addChild(this.arrowRight);
         }		
 		
