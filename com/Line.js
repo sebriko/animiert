c91f77622042443e17aa99a37866a6edd0daef58
sebriko <sebriko@freenet.de>
2024-04-02 22:08:34 +0200

    Line class extended

--- com/Line.js
+++ com/Line.js
@@ -1,4 +1,18 @@
+/**
+ * Represents a line.
+ * @extends {createjs.Container}
+ */
 export class Line extends createjs.Container {
+    /**
+     * Creates an instance of Line.
+     * @param {number} startX - The x-coordinate of the starting point of the line.
+     * @param {number} startY - The y-coordinate of the starting point of the line.
+     * @param {number} endX - The x-coordinate of the ending point of the line.
+     * @param {number} endY - The y-coordinate of the ending point of the line.
+     * @param {string} [color=createjs.Graphics.getRGB(255, 0, 0)] - The color of the line.
+     * @param {number} [thickness=1] - The thickness of the line.
+     * @param {Array<number>} [strokeDash=null] - The pattern of dashes and gaps.
+     */
     constructor(startX, startY, endX, endY, color = createjs.Graphics.getRGB(255, 0, 0), thickness = 1, strokeDash = null) {
         super();
         this.color = color;
@@ -8,16 +22,20 @@ export class Line extends createjs.Container {
         this.endY = endY;
         this.rotation = 0;
         this.thickness = thickness;
-        this.strokeDash = strokeDash; // Array für das Strichmuster
+        this.strokeDash = strokeDash; // Array for stroke pattern
         this.originalRotation = 0;
         this.setRotation = 0;
         this.line = new createjs.Shape();
         this.addChild(this.line);
 
         this.drawLine();
-        stage.addChild(this); // Füge den Container zur Bühne hinzu
+        stage.addChild(this); // Add the container to the stage
     }
 
+    /**
+     * Draws the line based on its properties.
+     * @private
+     */
     drawLine() {
         this.line.graphics.clear();
         this.line.graphics.beginStroke(this.color);
@@ -42,6 +60,13 @@ export class Line extends createjs.Container {
         stage.update();
     }
 
+    /**
+     * Sets the starting and ending coordinates of the line.
+     * @param {number} startX - The x-coordinate of the starting point of the line.
+     * @param {number} startY - The y-coordinate of the starting point of the line.
+     * @param {number} endX - The x-coordinate of the ending point of the line.
+     * @param {number} endY - The y-coordinate of the ending point of the line.
+     */
     setStartEnd(startX, startY, endX, endY) {
         this.startX = startX;
         this.startY = startY;
@@ -50,7 +75,51 @@ export class Line extends createjs.Container {
 
         this.drawLine();
     }
+	
+	/**
+     * Sets the x-coordinate of the starting point of the line.
+     * @param {number} startX - The x-coordinate of the starting point of the line.
+     */
+    setStartX(startX) {
+        this.startX = startX;
 
+        this.drawLine();
+    }
+	
+	/**
+     * Sets the y-coordinate of the starting point of the line.
+     * @param {number} startY - The y-coordinate of the starting point of the line.
+     */
+    setStartY(startY) {
+        this.startY = startY;
+
+        this.drawLine();
+    }
+	
+	/**
+     * Sets the x-coordinate of the ending point of the line.
+     * @param {number} endX - The x-coordinate of the ending point of the line.
+     */
+    setEndX(endX) {
+        this.endX = endX;
+
+        this.drawLine();
+    }
+	
+	/**
+     * Sets the y-coordinate of the ending point of the line.
+     * @param {number} endY - The y-coordinate of the ending point of the line.
+     */
+    setEndY(endY) {
+        this.endY = endY;
+
+        this.drawLine();
+    }
+
+    /**
+     * Sets the rotation of the line.
+     * @param {number} rotation - The rotation angle in degrees.
+     */
     rotation(rotation) {
         this.setRotation = rotation;
 
