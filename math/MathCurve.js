68d8baa50d32f6530922836e47e2fe07acab8231
sebriko <sebriko@freenet.de>
2024-04-06 13:57:59 +0200

    Curve class extended

--- math/MathCurve.js
+++ math/MathCurve.js
@@ -1,44 +1,91 @@
-/**
- * Represents a mathematical curve.
- * @extends {createjs.Shape}
- */
-class MathCurve extends createjs.Shape {
-    /**
-     * Creates an instance of MathCurve.
-     * @param {Function} equation The equation representing the curve.
-     * @param {string} color The color of the curve.
-     */
-    constructor(equation, color) {
+export class MathCurve extends createjs.Container {
+    constructor(equation, startX, endX, step, scaleValueX, color=createjs.Graphics.getRGB(255, 0, 0), thickness=1) {
         super();
-        this.graphics.setStrokeStyle(2); // Set the stroke width to 2
+        this.equation = equation;
+        this.color = color;
+        this.startX = startX;
+        this.endX = endX;
+        this.scaleValueX = scaleValueX;
+		this.markedX = 0;
+		this.markerInitialized = false;
+		this.markColor = createjs.Graphics.getRGB(0, 0, 0);
+		this.markRadius = 3;
+        this.thickness = thickness;
+this.step = step;
+        // Erstelle die Kurve
+        this.curve = new createjs.Shape();
+        this.curve.graphics.setStrokeStyle(this.thickness);
+        this.addChild(this.curve);
 
-        // Pass the equation and color to the drawCurve method
-        this.drawCurve(equation, color);
+        // Erstelle den Kreis
+        this.markedCircle = new createjs.Shape();
+        this.markedCircle.graphics.setStrokeStyle(5);
+        this.addChild(this.markedCircle);
+
+        this.drawCurve();
+        stage.addChild(this); // Füge den Container zur Bühne hinzu
     }
 
-    /**
-     * Draws a mathematical curve based on the provided equation.
-     * @param {Function} equation The equation representing the curve.
-     * @param {string} color The color of the curve.
-     */
-    drawCurve(equation, color) {
-        this.graphics.clear(); // Clear previous graphics
-        this.graphics.beginStroke(color); // Set the stroke color
+    drawCurve() {
+        this.curve.graphics.clear();
+        this.curve.graphics.beginStroke(this.color);
+        this.curve.graphics.setStrokeStyle(this.thickness);
 
-        // Draw the curve based on the equation
-        for (let x = 0; x <= stage.canvas.width; x += 1) {
-            const y = equation(x);
-            if (x === 0) {
-                this.graphics.moveTo(x, y);
+        for (let x = this.startX; x <= this.endX; x += this.step) {
+            const y = -this.equation(x);
+		
+            if (x === this.startX) {
+                this.curve.graphics.moveTo(x * this.scaleValueX, y);
             } else {
-                this.graphics.lineTo(x, y);
+                this.curve.graphics.lineTo(x * this.scaleValueX, y);
             }
         }
 
-        this.graphics.endStroke(); // End the stroke
+        this.curve.graphics.endStroke();
+        stage.update();
+    }
 
-        // Add the curve to the stage
-        stage.addChild(this);
-        stage.update(); // Update the stage to see the changes
+    setStartX(startX) {
+        this.startX = startX;
+        this.drawCurve();
+    }
+
+    setEndX(endX) {
+        this.endX = endX;
+        this.drawCurve();
+    }
+
+    setStrokeThickness(thickness) {
+        this.thickness = thickness;
+        this.curve.graphics.setStrokeStyle(this.thickness);
+        this.drawCurve();
+    }
+
+    setMarkProperties(color, radius) {
+        this.markerInitialized = true;
+        this.markColor = color || this.markColor;
+        this.markRadius = radius || this.markRadius;
+
+        this.markedCircle.graphics.clear();
+        this.markedCircle.graphics.beginFill(this.markColor)
+            .drawCircle(0, 0, this.markRadius)
+            .endFill();
+
+        stage.update();
+    }
+
+    mark(x=this.markedX) {
+        if (!this.markerInitialized) {
+            this.setMarkProperties();
+        }
+
+        const y = this.equation(x);
+
+        this.markedCircle.x = x * this.scaleValueX;
+        this.markedCircle.y = y;
+
+        this.markedX = x;
+
+        stage.update();
     }
 }
