b5b4ca6400426e98d245c8fc607d98663b1ab1e5
sebriko <sebriko@freenet.de>
2024-12-20 23:11:28 +0100

    Percantage values can be used now to mark a value

--- math/MathCurve.js
+++ math/MathCurve.js
@@ -1,20 +1,6 @@
 import { Line } from '../com/Line.js'; 
 
-/**
- * MathCurve class represents a mathematical curve drawn on the stage.
- * @extends createjs.Container
- */
 export class MathCurve extends createjs.Container {
-    /**
-     * Creates an instance of MathCurve.
-     * @param {Function} equation - The mathematical equation defining the curve.
-     * @param {number} startX - The starting value of x.
-     * @param {number} endX - The ending value of x.
-     * @param {number} step - The step size for incrementing x.
-     * @param {number} scaleValueX - The scaling factor for x-axis.
-     * @param {string} [color=createjs.Graphics.getRGB(255, 0, 0)] - The color of the curve.
-     * @param {number} [thickness=1] - The thickness of the curve.
-     */
     constructor(equation, startX, endX, step, scaleValueX, color=createjs.Graphics.getRGB(255, 0, 0), thickness=1) {
         super();
         this.equation = equation;
@@ -28,12 +14,11 @@ export class MathCurve extends createjs.Container {
         this.markRadius = 3;
         this.thickness = thickness;
         this.step = step;
-        // Erstelle die Kurve
+
         this.curve = new createjs.Shape();
         this.curve.graphics.setStrokeStyle(this.thickness);
         this.addChild(this.curve);
 
-        // Erstelle den Kreis
         this.markedCircle = new createjs.Shape();
         this.markedCircle.graphics.setStrokeStyle(5);
         this.addChild(this.markedCircle);
@@ -48,10 +33,6 @@ export class MathCurve extends createjs.Container {
         stage.addChild(this); 
     }
 
-    /**
-     * Draws the curve based on the provided equation and parameters.
-     * @private
-     */
     drawCurve() {
         this.curve.graphics.clear();
         this.curve.graphics.beginStroke(this.color);
@@ -59,7 +40,6 @@ export class MathCurve extends createjs.Container {
 
         for (let x = this.startX; x <= this.endX; x += this.step) {
             const y = this.equation(x);
-
             if (x === this.startX) {
                 this.curve.graphics.moveTo(x * this.scaleValueX, y);
             } else {
@@ -71,11 +51,6 @@ export class MathCurve extends createjs.Container {
         stage.update();
     }
 
-    /**
-     * Sets the properties of the marker for specific points on the curve.
-     * @param {string} [color] - The color of the marker.
-     * @param {number} [radius] - The radius of the marker.
-     */
     setMarkProperties(color, radius) {
         this.markerInitialized = true;
         this.markColor = color || this.markColor;
@@ -88,41 +63,35 @@ export class MathCurve extends createjs.Container {
 
         stage.update();
     }
-	
-	
-	/**
-	 * Returns the y-value for a given x-value based on the mathematical equation of the curve.
-	 * @param {number} x - The x-value for which the y-value is to be calculated.
-	 * @returns {number} - The calculated y-value.
-	 */
-	getValue(x) {
-		const y = this.equation(x);
-		return y;
-	}
 
-    /**
-     * Draws guidelines at a specified x-coordinate on the curve.
-     * @param {number} [x=this.markedX] - The x-coordinate where guidelines should be drawn.
-     */
-    drawMarker(x=this.markedX, withLines=true) {
+    getValue(x) {
+        return this.equation(x);
+    }
+
+    getValueByPercentage(percent) {
+        const clampedPercent = Math.max(0, Math.min(100, percent));
+        const x = this.startX + (clampedPercent / 100) * (this.endX - this.startX);
+        return this.getValue(x);
+    }
+
+    drawMarker(value = this.markedX, isPercentage = false, withLines = true) {
         if (!this.markerInitialized) {
             this.setMarkProperties();
         }
 
-        this.markedX = x;
-        const markedY = this.equation(this.markedX); // Calculate the result of the equation
+        this.markedX = isPercentage ? this.startX + (Math.max(0, Math.min(100, value)) / 100) * (this.endX - this.startX) : value;
+        const markedY = this.equation(this.markedX);
         const startX = this.markedX;
         const startY = 0;
         const endX = this.markedX;
         const endY = markedY;
 
-        this.markedCircle.x = x;
+        this.markedCircle.x = this.markedX * this.scaleValueX;
         this.markedCircle.y = markedY;
 
-        // Update the coordinates of the line
-		if (withLines===true) {
-        this.vGuideLine.setStartEnd(startX, startY, endX, endY);
-		this.hGuideLine.setStartEnd(0, endY, endX, endY);
-		}
+        if (withLines) {
+            this.vGuideLine.setStartEnd(startX, startY, endX, endY);
+            this.hGuideLine.setStartEnd(0, endY, endX, endY);
+        }
     }
 }
